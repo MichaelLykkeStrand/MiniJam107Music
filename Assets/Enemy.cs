aadb6946eb0f3615bc6d24598cb5a9939607b063
Michael Strand <32778224+MichaelLykkeStrand@users.noreply.github.com>
2022-05-27 23:22:05 +0200

    Update Enemy.cs

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -4,5 +4,11 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public int speed = 10;
+    public int damage = 1;
 
+    private void FixedUpdate()
+    {
+        this.transform.position -= (Vector3)Vector2.right * 0.001f * speed;
+    }
 }
